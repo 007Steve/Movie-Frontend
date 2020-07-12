a9f82f27578866d1871eb0b2c397b92b8547829a
007steve <stephen.plummer1.sp@gmail.com>
2020-07-12 00:59:32 -0400

    added fetch

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// this is getting all the data 
+fetch("http://localhost:3000/movies")
+.then(res => res.json())
+.then(movies => {
+ movies.forEach(movie => {
+     const {id,title,description,year,image,category} = movie
+     new Movie(id,title,description,year,image,category)
+ });
+
+})
