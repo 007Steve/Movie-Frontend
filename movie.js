b1616f1a1dce36597bc83ab93dabd2edea57eb28
007steve <stephen.plummer1.sp@gmail.com>
2020-07-12 01:04:46 -0400

    added constructor to movie class

---
+++ movie.js
@@ -0,0 +1,14 @@
+class Movie {
+
+constructor(id,title,description,year,image,category){
+    this.id = id
+    this.title = title
+    this.description = description
+    this.year = year
+    this.image = image
+    this.category = category
+}
+
+
+
+}
