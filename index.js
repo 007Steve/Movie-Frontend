8ab051b06cb61031cf6263d02d78e20454f96eb4
007steve <stephen.plummer1.sp@gmail.com>
2020-07-12 03:10:45 -0400

    fixed forEach

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 fetch("http://localhost:3000/movies")
 .then(res => res.json())
 .then(movies => {
+    
  movies.forEach(movie => {
-     const {id,title,description,year,image,category} = movie
-     new Movie(id,title,description,year,image,category)
+     const { id, title, description, year, image, category} = movie
+     
+     new Movie(id, title, description, year, image, category)
  });
 
 })
